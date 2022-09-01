33477c6066a1e113d3864cdbcaf2af68d923bdd0
safimuhammad <safiwahid38@gmail.com>
2022-09-02 02:32:22 +0500

    adding sin wave

--- karplusStrong.py
+++ karplusStrong.py
@@ -1,1 +1,13 @@
 #This project will use karplus strong algorithm to generate musical overtones.
+import wave, math 
+import numpy as np
+
+sRate = 44100
+nSamples = sRate * 5
+x = np.arange(nSamples)/float(sRate)
+vals = np.sin(2.0*math.pi*220.0*x)
+data = np.array(vals*32767, 'int16').tostring()
+file = wave.open('sine220.wav','wb')
+file.setparams((1, 2, sRate, nSamples, 'NONE', 'uncompressed'))
+file.writeframes(data)
+file.close() 
