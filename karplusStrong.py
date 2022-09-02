09a6989f02faba3700ab4c8044c6fdc668fb6608
safimuhammad <safiwahid38@gmail.com>
2022-09-03 03:09:23 +0500

    planning next phase

--- karplusStrong.py
+++ karplusStrong.py
@@ -7,7 +7,10 @@ nSamples = sRate * 5
 x = np.arange(nSamples)/float(sRate)
 vals = np.sin(2.0*math.pi*220.0*x)
 data = np.array(vals*32767, 'int16').tostring()
+print(data)
 file = wave.open('sine220.wav','wb')
 file.setparams((1, 2, sRate, nSamples, 'NONE', 'uncompressed'))
 file.writeframes(data)
 file.close() 
+
+# we will now how sound is simulated using karplus-strong algorithm
